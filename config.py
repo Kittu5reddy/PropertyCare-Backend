8c2ca242c822377c0962f9eef03acff56422b76a
kittu5reddy <kaushikpalvai2004@gmail.com>
2025-07-07 19:28:21 +0530

    connect

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import os
 from typing import List
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
-from pydantic import  Field
 
 # Load the correct .env file
 env = os.getenv("ENV", "dev")
 load_dotenv(f".env.{env}")
 load_dotenv()
-
 class Settings(BaseSettings):
     # JWT
     REFRESH_TOKEN_SECRET_KEY: str
@@ -16,15 +14,15 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
-    ALLOWED_PDF_EXTENSIONS: List[str] = Field(default_factory=list)
-    ALLOWED_IMAGE_EXTENSIONS: List[str] = Field(default_factory=list)
-    ALLOWED_VIDEO_EXTENSIONS: List[str] = Field(default_factory=list)
+
     # File Config
     BASE_DOCUMENT_PATH: str
     MAX_SIZE_PDF: int
     MAX_SIZE_IMAGE: int
     MAX_SIZE_VIDEO: int
-
+    ALLOWED_PDF_EXTENSIONS: List[str]
+    ALLOWED_IMAGE_EXTENSIONS: List[str]
+    ALLOWED_VIDEO_EXTENSIONS: List[str]
     SUBFOLDERS: dict[str, str] = {
         "aadhar": "aadhar",
         "pan": "pan",
