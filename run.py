72ddd8c1729649f86e1b11b321567be19aa29070
root <root@srv933255.hstgr.cloud>
2025-07-30 02:16:57 +0000

    jdsf

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ import asyncio
 
 if __name__=="__main__":
     asyncio.run(init_models())
-    uvicorn.run("run:app", host= "127.0.0.1", port= 8000,reload=True)
+    uvicorn.run("run:app", host= "0.0.0.0", port= 8000)
