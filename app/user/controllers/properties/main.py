e17d6df76f8c32cf4f133b46479bf85bc78c9e85
Palvai Kaushik Reddy <kaushikpalvai2004@gmail.com>
2025-08-30 00:53:50 +0530

    prop-detail

--- app/user/controllers/properties/main.py
+++ app/user/controllers/properties/main.py
@@ -211,3 +211,42 @@ async def get_property_info(
     except Exception as e:
         print(str(e))
         raise HTTPException(status_code=500, detail=f"Failed to fetch property info: {str(e)}")
+
+
+
+@prop.get("/get-reference-images/{property_id}")
+async def get_reference_images(
+    property_id: str,
+    token:str=Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+):
+    try:
+        objects = await list_s3_objects(prefix=f"property/{property_id}/property_photos/")
+        # print(objects)
+        # Convert S3 keys to signed/public URLs if needed
+        image_urls = list(map(get_image,objects))
+
+        return {
+            "property_id": property_id,
+            "images": image_urls
+        }
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=f"Failed to fetch reference images: {str(e)}")
+@prop.get("/get-property-documents/{property_id}")
+async def get_reference_images(
+    property_id: str,
+    token:str=Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+):
+    try:
+        objects = await list_s3_objects(prefix=f"property/{property_id}/legal_documents/")
+        image_urls = list(map(get_image,objects))
+
+        return {
+            "property_id": property_id,
+            "images": image_urls
+        }
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=f"Failed to fetch reference images: {str(e)}")
