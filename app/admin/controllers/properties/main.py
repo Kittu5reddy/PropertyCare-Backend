349442bcb651d17a229c3762327e8868f46cc2b4
kittu5reddy <kaushikpalvai2004@gmail.com>
2026-01-26 15:42:26 +0530

    properties user and about added

--- app/admin/controllers/properties/main.py
+++ app/admin/controllers/properties/main.py
@@ -7,48 +7,162 @@ from app.core.services.db import AsyncSession,get_db
 admin_properties=APIRouter(prefix='/admin_properties',tags=['Admin Properties'])
 from sqlalchemy import select
 from app.core.services.s3 import get_image_cloudfront_signed_url
-
-
+from jose import JWTError
+from app.user.models.users import User
+from app.user.models.personal_details import PersonalDetails
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 @admin_properties.get("/get-properties")
 async def get_properties(
-    token: str = Depends(oauth2_scheme),
+    # token: str = Depends(oauth2_scheme),
     db: AsyncSession = Depends(get_db),
     start: int = 0,
     limit: int = 10
 ):
-    # 🔒 Auth check
-    admin = await get_current_admin(token=token, db=db)
+    try:
+        # 🔒 Auth check
+        # admin = await get_current_admin(token=token, db=db)
 
 
-    stmt = (
-        select(PropertyDetails)
-        .order_by(PropertyDetails.id.desc())  # 👈 important
-        .offset(start)
-        .limit(limit)
-    )
+        stmt = (
+            select(PropertyDetails)
+            .order_by(PropertyDetails.id.desc())  # 👈 important
+            .offset(start)
+            .limit(limit)
+        )
 
-    result = await db.execute(stmt)
-    properties = result.scalars().all()
-    response = [
-    {
-        "image_url": await get_image_cloudfront_signed_url(f"/property/{record.property_id}/legal_documents/property_photo.png"),
-        "is_active": record.active_sub,
-        "property_id": record.property_id,
-        "property_name": record.property_name,
-        "user_id": record.user_id
-    }
-    for record in properties
-]
+        result = await db.execute(stmt)
+        properties = result.scalars().all()
+        response = [
+        {
+            "image_url": await get_image_cloudfront_signed_url(f"/property/{record.property_id}/legal_documents/property_photo.png"),
+            "is_active": record.active_sub,
+            "property_id": record.property_id,
+            "property_name": record.property_name,
+            "user_id": record.user_id
+        }
+        for record in properties
+]   
 
-    
-    return {
-        "start": start,
-        "limit": limit,
-        "count": len(properties),
-        "data": response
-    }
 
+        return {
+            "start": start,
+            "limit": limit,
+            "count": len(properties),
+            "data": response
+        }
+    except HTTPException as e:
+        raise e 
+    except JWTError as e:
+        raise HTTPException(status_code=401,detail="Token Expired")
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500,detail=str(e))
+
+
+
+@admin_properties.get("/get-user-details")
+async def get_user_details(
+    user_id: str,
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+):
+        
+    try:
+        admin = await get_current_admin(token=token, db=db)
+        stmt = await db.execute(
+            select(PersonalDetails)
+            .where(PersonalDetails.user_id == user_id)
+        )
+
+        user = stmt.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(
+                status_code=404,
+                detail="User Details Not Found"
+            )
+
+        return {
+            "user_id": user.user_id,
+            "full_name": f"{user.first_name or ''} {user.last_name or ''}".strip(),
+            "user_name": user.user_name,
+            "contact_number": user.contact_number,
+            "location": user.city,
+            # ✅ date only
+            "member_from": user.created_at.date() if user.created_at else None,
+            "nri": user.nri
+        }
+    except HTTPException as e:
+        raise e 
+    except JWTError as e:
+        raise HTTPException(status_code=401,detail="Token Expired")
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500,detail=str(e))
+
+
+
+
+@admin_properties.get("/get-property-detail")
+async def get_property_detail(
+    property_id:str,
+    # token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db),
+
+):
+    try:
+        # admin = await get_current_admin(token=token, db=db)
+
+        stmt = await db.execute(
+            select(PropertyDetails)
+            .where(PropertyDetails.property_id == property_id)
+        )
+
+        property = stmt.scalar_one_or_none()
+
+        if not property:
+            raise HTTPException(
+                status_code=404,
+                detail="Property Details Not Found"
+            )
+
+        return {
+            "property_id": property.property_id,
+            "plot_number": property.plot_number,
+            "project_name_or_venture": property.project_name_or_venture,
+            "house_number": property.house_number,
+            "street": property.street,
+            "district": property.district,
+            "mandal": property.mandal,
+            "city": property.city,
+            "pin_code": property.pin_code,
+            "facing": property.facing,
+            "state": property.state,
+            "rental_income": property.rental_income,
+            "physical_verification": property.is_verified,
+            "type": property.type,
+            "sub_type": property.sub_type,
+            "scale": property.scale,
+            "alternate_name": property.alternate_name,
+            "alternate_contact": property.alternate_contact,
+            "image_url": await get_image_cloudfront_signed_url(f"/property/{property.property_id}/legal_documents/property_photo.png"),
+      
+            "is_active": property.active_sub
+        }
+    except HTTPException as e:
+        raise e 
+    except JWTError as e:
+        raise HTTPException(status_code=401,detail="Token Expired")
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500,detail=str(e))
+
+# user details
+
+ 
+# prop details
+
+ 
